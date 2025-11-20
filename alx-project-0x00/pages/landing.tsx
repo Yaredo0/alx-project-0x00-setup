706b5b82b1014ac7717cf09ef5210525559026b1
Yaredo0 <yared4sy@gmail.com>
2025-11-20 13:09:31 +0300

    4

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,13 @@
+import React from "react";
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
+    <div className="p-6">
+      <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
