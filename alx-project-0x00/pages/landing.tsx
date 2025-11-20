367db63b88efb2daa8f927d704859687802b71a3
Yaredo0 <yared4sy@gmail.com>
2025-11-20 13:15:06 +0300

    5

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -12,6 +12,7 @@ const Landing: React.FC = () => {
 
       <div className="mt-6 flex gap-4">
         <Button styles="rounded-lg" />
+        <Button styles="rounded-sm" />
         <Button styles="rounded-md" />
         <Button styles="rounded-full" />
       </div>
